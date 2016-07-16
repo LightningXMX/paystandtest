03203d616b5ad939754d46d5c8af3e02a32696e1
lightningxmx <lightningxmx@163.com>
2016-07-16 17:37:45 +0800

    init

---
+++ src/main/java/PeopleMapper.java
@@ -0,0 +1,8 @@
+/**
+ * Created by mingxin on 16/7/13.
+ */
+public interface PeopleMapper {
+
+    People selectPeopleById(long id);
+
+}
---
+++ src/main/java/Test.java
@@ -0,0 +1,37 @@
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by mingxin on 16/7/13.
+ */
+public class Test {
+    public static void main(String[] args) {
+
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = null;
+        try {
+            inputStream = Resources.getResourceAsStream(resource);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        SqlSession session = sqlSessionFactory.openSession();
+        try {
+            PeopleMapper mapper = session.getMapper(PeopleMapper.class);
+            People people = mapper.selectPeopleById(1L);
+            System.out.println(people);
+        } catch (Throwable e) {
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+
+
+    }
+}
